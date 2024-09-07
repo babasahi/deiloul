a27d58197847f0d16ed8f15329789b432f565a31
babe-saleh-mahfoud <babesalehmahfoud@gmail.com>
2024-09-07 20:08:15 +0000

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:deiloul/backend.dart';
-import 'package:deiloul/prompt.dart';
+import 'package:deiloul/services/backend.dart';
+import 'package:deiloul/models/prompt.dart';
+import 'package:deiloul/views/chat_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,68 +15,12 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
-  final TextEditingController _promptController = TextEditingController();
-  String _answer = '';
+  final String _answer = '';
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                Container(
-                  width: 300,
-                  height: 400,
-                  decoration: BoxDecoration(
-                    color: Colors.amber.shade100,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Text(_answer),
-                ),
-                const SizedBox(height: 120),
-                Container(
-                  width: 300,
-                  decoration: BoxDecoration(
-                    color: Colors.amber.shade100,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: TextField(
-                    controller: _promptController,
-                    decoration: const InputDecoration(
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                GestureDetector(
-                  onTap: () async {
-                    await callModel(
-                      Prompt(
-                          message: _promptController.text.trim(),
-                          user: 'saleh',
-                          date: DateTime.now()),
-                    ).then((v) {
-                      setState(() {
-                        if (v != null) {
-                          _answer = v.answer;
-                        }
-                      });
-                    });
-                  },
-                  child: const SizedBox(
-                    height: 22,
-                    width: 88,
-                    child: Icon(
-                      Icons.telegram,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ChatScreen(),
     );
   }
 }
---
+++ lib/views/chat_screen.dart
@@ -0,0 +1,138 @@
+import 'dart:math';
+
+import 'package:deiloul/models/prompt.dart';
+import 'package:deiloul/models/prompt_answer.dart';
+import 'package:deiloul/services/backend.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  List<PromptAnswer> answers = [];
+  final TextEditingController _promptController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.7,
+              child: ListView(
+                children: answers
+                    .map((answer) => PromptAnswerWidget(promptAnswer: answer))
+                    .toList(),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.09,
+              child: Container(
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                    color: Colors.amber.shade100,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: Colors.grey,
+                    )),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      height: MediaQuery.of(context).size.height * 0.08,
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () async {
+                        /*
+                        await callModel(
+                          Prompt(
+                              message: _promptController.text.trim(),
+                              user: 'saleh',
+                              date: DateTime.now()),
+                        ).then((v) {
+                          setState(() {
+                            if (v != null) {
+                              messages.add(
+                                  '${v.answer} \n ${v.model} \n ${v.dateTime?.toIso8601String()}');
+                              _promptController.clear();
+                            }
+                          });
+                        });
+                        */
+                        setState(() {
+                          answers.add(PromptAnswer(
+                            answer: 'New message' * Random.secure().nextInt(8),
+                            dateTime: DateTime.now(),
+                            model: null,
+                          ));
+                        });
+                      },
+                      icon: const Icon(
+                        Icons.send,
+                        color: Colors.green,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PromptAnswerWidget extends StatelessWidget {
+  const PromptAnswerWidget({super.key, required this.promptAnswer});
+  final PromptAnswer promptAnswer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        vertical: 12,
+        horizontal: 8,
+      ),
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        children: [
+          Text(
+            promptAnswer.answer,
+          ),
+          const SizedBox(height: 8),
+          const Row(
+            children: [
+              Icon(
+                FontAwesomeIcons.volumeXmark,
+              ),
+              Icon(
+                FontAwesomeIcons.soundcloud,
+              ),
+              Icon(
+                FontAwesomeIcons.soundcloud,
+              ),
+              Icon(
+                FontAwesomeIcons.soundcloud,
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
